cff7f2341cf11043facd2907fb8511359a424a8d
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-04-28 11:47:23 +0300

    Рефакторинг - Шаблон Декоратор для валидатора

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -109,7 +109,8 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Input validate = new ValidateInput();
+        Input input = new ConsoleInput();
+        Input validate = new ValidateInput(input);
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateItem(), new ShowAll(), new ReplaceItem(), new DeleteItem(), new FindItemById(),
