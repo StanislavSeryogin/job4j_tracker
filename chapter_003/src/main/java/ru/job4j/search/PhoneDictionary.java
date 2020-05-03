48f239d755237894c285fc2dea79b23c4586f9d6
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-05-03 11:58:09 +0300

    Телефонный справочник на базе ArrayList

---
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,24 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        var result = new ArrayList<Person>();
+        for (var person : persons) {
+            if (person.getName().contains(key)
+                    || person.getAddress().contains(key)
+                    || person.getPhone().contains(key)
+                    || person.getSurname().contains(key)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
