3132f55b53d0f0eaff1bccecacaca834ba1fa60a
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-05-29 17:38:14 +0300

    0. Сортировка 001

--- chapter_002/src/main/java/ru/job4j/tracker/ItemSortMinus.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ItemSortMinus.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class ItemSortMinus implements Comparator<Item> {
     @Override
     public int compare(Item first, Item second) {
-        return -1 * first.getName().compareTo(second.getName());
+        return second.getName().compareTo(first.getName());
     }
 }
