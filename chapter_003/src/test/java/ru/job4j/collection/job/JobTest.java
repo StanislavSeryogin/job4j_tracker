bfbabaedd06735531d32aa312efa136aeb6264d1
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-05-31 14:07:57 +0300

    Сортировка User с использованием Comparator

---
+++ chapter_003/src/test/java/ru/job4j/collection/job/JobTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.collection.job;
+
+import org.junit.Test;
+import java.util.*;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    @Test
+    public void getNameAndPriority() {
+        Comparator<Job> cmp = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmp.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void getPriorityAndName() {
+        Comparator<Job> cmp = new JobAscByPriority().thenComparing(new JobDescByName());
+        int rsl = cmp.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void getAscByName() {
+        Comparator<Job> cmp = new JobAscByName();
+        int rsl = cmp.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void getDescByName() {
+        Comparator<Job> cmp = new JobDescByName();
+        int rsl = cmp.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void getAscByPriority() {
+        Comparator<Job> cmp = new JobAscByPriority();
+        int rsl = cmp.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void getDescByPrioruty() {
+        Comparator<Job> cmp = new JobDescByPriority();
+        int rsl = cmp.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+}
