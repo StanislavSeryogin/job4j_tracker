4c7edc51373fe7832c036ca5d2cd400b1cfd03d7
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-06-06 15:15:47 +0300

    Отсортировать департаменты

---
+++ chapter_003/src/main/java/ru/job4j/collection/departments/DepDescComp.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection.departments;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+
+        int res = 0;
+        int size = Math.min(o1.length(), o2.length());
+        for (int i = 0; i < size && res == 0; i++) {
+            res = Character.compare(o2.charAt(i), o1.charAt(i));
+        }
+        if (res == 0) {
+            res = Integer.compare(o1.length(), o2.length());
+        }
+
+        return res;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection.departments;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class DepartmentsTest {
+
+    @Test
+    public void whenMissed() {
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        Departments.sortAsc(result);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChange() {
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        Departments.sortAsc(result);
+        assertThat(result, is(expect));
+    }
+}
