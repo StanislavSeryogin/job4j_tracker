bbe9fcd8eb9592c5816b72dd8bc0c13085d44595
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-06-07 15:17:27 +0300

    Отсортировать департаменты_2

--- chapter_003/src/main/java/ru/job4j/collection/departments/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/collection/departments/DepDescComp.java
@@ -8,8 +8,9 @@ public class DepDescComp implements Comparator<String> {
 
         int res = 0;
         int size = Math.min(o1.length(), o2.length());
-        for (int i = 0; i < size && res == 0; i++) {
+        for (int i = 0; i < size; i++) {
             res = Character.compare(o2.charAt(i), o1.charAt(i));
+            if (!(res == 0)) break;
         }
         if (res == 0) {
             res = Integer.compare(o1.length(), o2.length());
