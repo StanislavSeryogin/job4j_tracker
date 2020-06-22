32dff05013e8b7da420db48817d418eec9461483
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-06-22 16:50:19 +0300

    Profiles_Fix

--- chapter_004/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profiles.java
@@ -5,9 +5,18 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        List<Address> rsl = profiles.stream().map(
+        return profiles.stream().map(
                 Profile::getAddress
         ).collect(Collectors.toList());
-        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Profiles profiles = new Profiles();
+        List<Profile> profile = List.of(
+                new Profile(new Address("Moscow", "First", 11, 111)),
+                new Profile(new Address("Kiev", "Second", 22, 222)),
+                new Profile(new Address("Minsk", "Third", 33, 333))
+        );
+        profiles.collect(profile).forEach(System.out::println);
     }
 }
