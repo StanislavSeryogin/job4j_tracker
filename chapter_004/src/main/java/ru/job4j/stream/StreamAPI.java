29fc5b56c78fc1d26b272bebc3ac27de5d011115
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-06-22 11:01:42 +0300

    StreamAPI

---
+++ chapter_004/src/main/java/ru/job4j/stream/StreamAPI.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+public class StreamAPI {
+    public static class Task{
+        private final Integer num;
+
+        public Task(Integer num) {
+            this.num = num;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task(1),
+                new Task(-2),
+                new Task(7),
+                new Task(3),
+                new Task(0)
+        );
+
+        Integer nums = tasks.stream().map(
+                task -> task.num
+        ).reduce(0, Integer::sum);
+        System.out.println(nums);
+    }
+}
