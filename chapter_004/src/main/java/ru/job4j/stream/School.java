5491c56fbb665b7833b7e00eda9583c0a883524a
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-06-24 07:27:40 +0300

    ListToMap

--- chapter_004/src/main/java/ru/job4j/stream/School.java
+++ chapter_004/src/main/java/ru/job4j/stream/School.java
@@ -1,6 +1,6 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -11,5 +11,21 @@ public class School {
                 predict
         ).collect(Collectors.toList());
         return list;
+    }
+
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(
+                new Student("First", 100),
+                new Student("Second", 80),
+                new Student("Third", 30),
+                new Student("Fourth", 60),
+                new Student("Fifth", 10)
+        );
+        System.out.println(students.stream().distinct().collect(
+                Collectors.toMap(
+                        Student::getSurname,
+                        student -> (Object) student
+                ))
+        );
     }
 }
