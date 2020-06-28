c77f2376446286dd0850cedd18a19203a5bcad02
StanislavSeryogin <stas.seryogin.87@yandex.ru>
2020-06-28 07:29:23 +0300

    Тестовое задание из модуля коллекции Lite переделать на Stream API

--- chapter_003/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_003/src/main/java/ru/job4j/bank/BankService.java
@@ -11,43 +11,35 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
+        User user = this.findByPassport(passport);
         if (user != null) {
-            this.users.get(user).add(account);
+            users.get(user).add(account);
         }
-
     }
 
     public User findByPassport(String passport) {
-        User user = null;
-        for (User person: users.keySet()) {
-            user = person;
-            break;
-        }
-        return user;
+        return users.keySet().stream().filter(u -> u.getPassport().equals(passport)).
+                reduce(null, (s, n) -> n);
     }
 
     public Account findByRequisite(String passport, String requisite) {
         Account account = null;
         User user = this.findByPassport(passport);
-        List<Account> accounts = user != null ? this.users.get(user) : Collections.emptyList();
-        for (Account accountBank: accounts) {
-            if (accountBank.getRequisite().equals(requisite)) {
-                account = accountBank;
-                break;
-            }
+        if (user != null) {
+            account = users.get(user).stream().filter(req -> req.getRequisite().equals(requisite)).
+                    reduce(null, (s, n) -> n);
         }
         return account;
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
-                                 String destPassport, String dеstRequisite, double amount) {
+                                 String destPassport, String destRequisite, double amount) {
         boolean conf = false;
         User sender = findByPassport(srcPassport);
         User recipient = findByPassport(destPassport);
         if (sender != null && recipient != null) {
             int srcAccount = users.get(sender).indexOf(findByRequisite(srcPassport, srcRequisite));
-            int destAccount = users.get(recipient).indexOf(findByRequisite(destPassport, dеstRequisite));
+            int destAccount = users.get(recipient).indexOf(findByRequisite(destPassport, destRequisite));
             if (srcAccount != -1 && destAccount != -1) {
                 if (users.get(sender).get(srcAccount).getBalance() >= amount) {
                     double balance = users.get(sender).get(srcAccount).getBalance() - amount;
